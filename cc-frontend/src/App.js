24ec3521e39ffaaa6c4ff0a8b949c5998ad6cb58
IsaiahWitzke <isaiah.witzke@gmail.com>
2021-01-17 17:08:10 -0500

    frontend

---
+++ cc-frontend/src/App.js
@@ -0,0 +1,27 @@
+import logo from './cc-logo.png';
+import './App.css';
+import DisplayImage from './DisplayImage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <a
+          className="App-link"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Download the App
+        </a>
+      </header>
+      {/* <DisplayImage /> */}
+    </div>
+  );
+}
+
+export default App;
